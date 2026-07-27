11b26d28e9cd7587d7a557cc35da3cd0d2432504
tmkha89 <tmkha89@gmail.com>
2026-07-27 13:22:03 +0700

    active info form

--- frontend/src/pages/QueueSuccessPage.tsx
+++ frontend/src/pages/QueueSuccessPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-const isShowForm = false;
+const isShowForm = true;
 
 const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
 
